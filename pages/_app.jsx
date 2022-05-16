9af498f591650689305d4cbb7e9c07f643b37ac1
mohandast52 <mohandast52@gmail.com>
2022-05-16 16:26:37 +0530

    metadata image

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -46,7 +46,7 @@ class MyApp extends App {
           />
           <meta
             property="og:image"
-            content="https://www.elcollectooorr.art/images/1CollectAndChill/background.png"
+            content="https://www.elcollectooorr.art/images/1CollectAndChill/metadata-background.png"
           />
 
           <meta property="twitter:card" content="summary_large_image" />
