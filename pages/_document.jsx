3250ab9108899bc1030bb8001e0b91d87955eefb
mohandast52 <mohandast52@gmail.com>
2022-06-02 16:32:35 +0530

    update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,3 +1,33 @@
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+
+/*
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
@@ -33,3 +63,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+*/
