61aa6eba39dd42e42ed312c71df997b7a123c296
Ardian <ardianabazi09@gmail.com>
2022-06-19 18:38:08 +0200

    feat: added vault factory constants

--- common-util/AbiAndAddresses/vaultContract.jsx
+++ common-util/AbiAndAddresses/vaultContract.jsx
@@ -1,5 +1,4 @@
-// export const VAULT_ADDRESS = '0xADe6FDAba1643E4D1eeF68Da7170F234470938c6';
-export const VAULT_ADDRESS = '0xfcb51a6ac3330f60664adc6c310f6515f193c9d4'; // rinkeby
+export const VAULT_ADDRESS = '0x2F7e0C5a4CF1C42D15BE641b00CB2649B86c70Ca'; // rinkeby
 
 export const VAULT_CONTRACT = {
   abi: [
@@ -853,6 +852,273 @@ export const VAULT_CONTRACT = {
       outputs: [
         {
           internalType: 'address payable',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+};
+
+export const VAULT_FACTORY_ADDRESS = '0x2C03ca9fb5a7b5B26996c00F7c419C5E9C706196';
+
+export const VAULT_FACTORY_CONTRACT = {
+  abi: [
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_settings',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'token',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'id',
+          type: 'uint256',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'price',
+          type: 'uint256',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'vault',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'vaultId',
+          type: 'uint256',
+        },
+      ],
+      name: 'Mint',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'previousOwner',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'newOwner',
+          type: 'address',
+        },
+      ],
+      name: 'OwnershipTransferred',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+      name: 'Paused',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+      name: 'Unpaused',
+      type: 'event',
+    },
+    {
+      inputs: [],
+      name: 'logic',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'string',
+          name: '_name',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: '_symbol',
+          type: 'string',
+        },
+        {
+          internalType: 'address',
+          name: '_token',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: '_id',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: '_supply',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: '_listPrice',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: '_fee',
+          type: 'uint256',
+        },
+      ],
+      name: 'mint',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'owner',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'pause',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'paused',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'renounceOwnership',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'settings',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'newOwner',
+          type: 'address',
+        },
+      ],
+      name: 'transferOwnership',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'unpause',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'vaultCount',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      name: 'vaults',
+      outputs: [
+        {
+          internalType: 'address',
           name: '',
           type: 'address',
         },
