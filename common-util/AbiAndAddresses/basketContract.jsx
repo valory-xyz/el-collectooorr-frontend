92ff61ebe68b2265e76d672b235f66204d9dfa18
Ardian <ardianabazi09@gmail.com>
2022-06-19 18:37:50 +0200

    feat: added basket factory constants

--- common-util/AbiAndAddresses/basketContract.jsx
+++ common-util/AbiAndAddresses/basketContract.jsx
@@ -734,3 +734,55 @@ export const BASKET_CONTRACT = {
     },
   ],
 };
+
+export const BASKET_FACTORY_ADDRESS = '0x9623B3C78e77Ea8c1A544cB73108B04787f96b08';
+
+export const BASKET_FACTORY_CONTRACT = {
+  abi: [
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: '_address',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: '_creator',
+          type: 'address',
+        },
+      ],
+      name: 'NewBasket',
+      type: 'event',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      name: 'baskets',
+      outputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'createBasket',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ],
+};
