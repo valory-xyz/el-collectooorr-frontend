16ffcd71d52895e992a12aabba1d98574b5e82d4
Ardian <ardianabazi09@gmail.com>
2022-06-19 18:38:40 +0200

    feat: testnet support

--- util/constants.jsx
+++ util/constants.jsx
@@ -1,6 +1,6 @@
-export const CHAIN_ID = [1, 4];
+export const CHAIN_ID = [1, 4, 31337];
 
-export const SUPPORTED_NETWORKS = [1, 4];
+export const SUPPORTED_NETWORKS = [1, 4, 31337];
 
 export const CONSTANTS = {
   ETH_GETBALANCE: 'eth_getBalance',
@@ -12,9 +12,4 @@ export const URL = {
   ROOT: '/',
   VAULT: '/vaults',
 };
-
-export const VAULT_TOKEN_ID = '0xF03b9E6707C1E29442e56B15d708e388A71a7177';
-
 export const METAMASK_ERROR_MSG = 'Please install MetaMask browser extension';
-
-export const SEND_ETH_TO = '0x34E37b5307d89F1cb76E71519B1D08708c04552E';
