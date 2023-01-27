a0a9da9169043607fbef60df5442e878cdb6c742
mohandast52 <mohandast52@gmail.com>
2023-01-27 16:10:58 +0530

    push

--- components/Vault/WidgetFooter.jsx
+++ components/Vault/WidgetFooter.jsx
@@ -10,20 +10,62 @@ export const Div = styled.div`
   }
 `;
 
-const DotSpace = () => <>&nbsp;&nbsp;•&nbsp;&nbsp;</>;
+export const ExtraContent = styled.div`
+  display: flex;
+  align-items: center;
+  > div {
+    &:not(:last-child) {
+      margin-right: 1.5rem;
+    }
+  }
+`;
 
 const { Text } = Typography;
 
-const DOCS_URL = 'https://docs.autonolas.network/product/el-collectooorr';
+const DotSpace = () => <>&nbsp;&nbsp;•&nbsp;&nbsp;</>;
+
+const getList = (myList) => myList.map(
+  ({
+    id, text, redirectTo, isInternal = true,
+  }, index) => (
+    <Fragment key={id}>
+      <Text type="secondary">
+        <a
+          href={redirectTo}
+          target={isInternal ? '_self' : '_blank'}
+          rel="noreferrer"
+        >
+          {text}
+        </a>
+
+        {index !== myList.length - 1 && <>&nbsp;&nbsp;•&nbsp;&nbsp;</>}
+      </Text>
+    </Fragment>
+  ),
+);
+
+const DOCS_URL = 'https://docs.autonolas.network/product/mintkit/';
 const EL_COL_STATUS_INFO = [
-  { name: 'What is this?', link: DOCS_URL },
-  { name: 'Run the Code', link: `${DOCS_URL}#run-the-code` },
-  { name: 'Build your own', link: `${DOCS_URL}#build-your-own` },
+  { text: 'Run demo code', redirectTo: `${DOCS_URL}#demo` },
+  {
+    text: 'Get help building',
+    redirectTo: 'https://propel.valory.xyz',
+    isInternal: false,
+  },
 ];
 const EL_COL_STATUS_INFO_MOBILE = [
-  { name: 'Run Code', link: `${DOCS_URL}#run-the-code` },
-  { name: 'Build', link: `${DOCS_URL}#build-your-own` },
+  { name: 'Run Code', link: `${DOCS_URL}#demo` },
+  { name: 'Build', link: `${DOCS_URL}#build` },
 ];
+
+const BuiltWith = (
+  <>
+    BUILT WITH&nbsp;
+    <a href={DOCS_URL} rel="noreferrer">
+      MINTKIT
+    </a>
+  </>
+);
 
 const WidgetFooter = () => {
   const generateContent = (contentList) => contentList.map((contract, index) => (
@@ -40,7 +82,25 @@ const WidgetFooter = () => {
   return (
     <Div>
       <ServiceStatusInfo
-        extra={generateContent(EL_COL_STATUS_INFO)}
+        // extra={generateContent(EL_COL_STATUS_INFO)}
+        extra={(
+          <ExtraContent>
+            {[
+              {
+                id: 'code',
+                name: BuiltWith,
+                list: EL_COL_STATUS_INFO,
+              },
+            ].map((e) => (
+              <div key={e.id}>
+                <div>
+                  <Text className="status-sub-header">{e.name}</Text>
+                </div>
+                <div className="status-sub-content">{getList(e.list)}</div>
+              </div>
+            ))}
+          </ExtraContent>
+        )}
         extraMd={generateContent(EL_COL_STATUS_INFO_MOBILE)}
       />
     </Div>
